0d5b6d1e3de455627af2dc90483f131eb79555f0
Jan Sobocinski <jan.sobocinski@contractors.roche.com>
2020-06-07 22:21:36 +0200

    Spring initializer

---
+++ src/main/java/pl/alegol/AlegolAppApplication.java
@@ -0,0 +1,13 @@
+package pl.alegol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlegolAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlegolAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/alegol/AlegolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.alegol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlegolAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
